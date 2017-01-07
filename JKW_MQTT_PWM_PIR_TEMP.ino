4e66d079f22c6396fb6e6edc4c0de31c84dbef29
KoljaWindeler <Kolja.Windeler@gmail.com>
2017-01-07 18:58:45 +0100

    init

---
+++ JKW_MQTT_PWM_PIR_TEMP.ino
@@ -0,0 +1,415 @@
+/*
+   MQTT Light (with brightness) for Home-Assistant - NodeMCU (ESP8266)
+   https://home-assistant.io/components/light.mqtt/
+
+   Libraries :
+    - ESP8266 core for Arduino : https://github.com/esp8266/Arduino
+    - PubSubClient : https://github.com/knolleary/pubsubclient
+
+   Schematic :
+    - 
+
+   Configuration (HA) : 
+    light:
+      platform: mqtt
+      name: 'Office light'
+      state_topic: 'office/light'
+      command_topic: 'office/light/switch'
+      brightness_state_topic: 'office/light/brightness'
+      brightness_command_topic: 'office/light/brightness/set'
+      optimistic: false
+
+   Samuel M. - v1.0 - 08.2016
+   If you like this example, please add a star! Thank you!
+   https://github.com/mertenats/open-home-automation
+
+
+	// todo: button auf dem geraet
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <OneWire.h> 
+
+
+#define MQTT_VERSION MQTT_VERSION_3_1_1
+#define DEV "dev1"
+
+// Wifi: SSID and password
+const char*             WIFI_SSID         			= "[Redacted]";
+const char*             WIFI_PASSWORD     			= "[Redacted]";
+
+// MQTT: ID, server IP, port, username and password
+const PROGMEM char*     MQTT_CLIENT_ID    			= "office_light";
+const PROGMEM char*     MQTT_SERVER_IP    			= "[Redacted]";
+const PROGMEM uint16_t  MQTT_SERVER_PORT  			= 1883;
+const PROGMEM char*     MQTT_USER         			= "[Redacted]";
+const PROGMEM char*     MQTT_PASSWORD     			= "[Redacted]";
+
+// MQTT: topics
+const PROGMEM char*     MQTT_PWM_LIGHT_STATE_TOPIC		= DEV "/PWM_light";			// publish state here
+const PROGMEM char*     MQTT_PWM_LIGHT_COMMAND_TOPIC		= DEV "/PWM_light/switch"; 		// get command here
+
+const PROGMEM char*     MQTT_SIMPLE_LIGHT_STATE_TOPIC		= DEV "/simple_light";		// publish state here
+const PROGMEM char*     MQTT_SIMPLE_LIGHT_COMMAND_TOPIC		= DEV "/simple_light/switch"; 	// get command here
+
+const PROGMEM char* 	MQTT_MOTION_STATUS_TOPIC		= DEV "/motion/status";		// publish
+const PROGMEM char* 	MQTT_TEMPARATURE_TOPIC			= DEV "/temperature";			// publish
+const PROGMEM char* 	MQTT_HUMIDITY_TOPIC			= DEV "/humidity";			// publish
+
+const PROGMEM char*     MQTT_PWM_LIGHT_BRIGHTNESS_STATE_TOPIC   = DEV "/PWM_light/brightness";	// publish
+const PROGMEM char*     MQTT_PWM_LIGHT_BRIGHTNESS_COMMAND_TOPIC = DEV "/PWM_light/brightness/set";	// set value
+
+// payloads by default (on/off)
+const PROGMEM char*     STATE_ON          			= "ON";
+const PROGMEM char*     STATE_OFF         			= "OFF";
+
+// variables used to store the statea and the brightness of the light
+boolean 		m_pwm_light_state 			= false;
+boolean			m_simple_light_state 			= false;
+uint8_t 		m_light_brightness			= 255;
+
+// variables used to store the pir state
+uint8_t 		m_pir_state 				= LOW; // no motion detected
+uint8_t 		m_published_pir_state 			= LOW;
+uint8_t 		m_pir_value 				= 0;
+
+
+// pin used for the led (PWM)
+#define PWM_LIGHT_PIN 4
+#define SIMPLE_LIGHT_PIN 12 // scheinbar?!
+#define BUTTON_INPUT_PIN 0 // ja?
+// DHT - D1/GPIO5
+#define DHT 1
+#define DS 2
+
+#define DHT_DS_MODE DS
+#define DHT_PIN 6
+// DHT - D1/GPIO5
+#define PIR_PIN 5
+
+// buffer used to send/receive data with MQTT
+const uint8_t 		MSG_BUFFER_SIZE 	= 60;
+char 			m_msg_buffer[MSG_BUFFER_SIZE];
+
+WiFiClient 		wifiClient;
+PubSubClient 		client(wifiClient);
+
+//Temperature chip i/o
+#if DHT_DS_MODE == DHT
+	#include "DHT.h"
+	DHT 		dht(DHT_PIN, DHT22); // DHT22
+#else
+	OneWire 	ds(DHT_PIN);  // on digital pin DHT_PIN
+#endif
+
+#define UPDATE_TEMP 60000 // once per minute
+uint32_t 		timer	= 0;
+
+//////////////////////////////////// SETTER ///////////////////////////////////////
+// function called to adapt the brightness and the state of the led
+void setPWMLightState() {
+	if (m_pwm_light_state) {
+		analogWrite(PWM_LIGHT_PIN, m_light_brightness);
+		Serial.print("INFO: PWM Brightness: ");
+		Serial.println(m_light_brightness);
+	} else {
+		analogWrite(PWM_LIGHT_PIN, 0);
+		Serial.println("INFO: Turn PWM light off");
+	}
+}
+// function called to adapt the state of the led
+void setSimpleLightState() {
+	if (m_simple_light_state) {
+		digitalWrite(SIMPLE_LIGHT_PIN, HIGH);
+		Serial.println("INFO: Simple pin on");
+	} else {
+		digitalWrite(SIMPLE_LIGHT_PIN, LOW);
+		Serial.println("INFO: Simple light off");
+	}
+}
+//////////////////////////////////// SETTER ///////////////////////////////////////
+
+//////////////////////////////////// PUBLISHER ///////////////////////////////////////
+// function called to publish the state of the led (on/off)
+void publishPWMLightState() {
+	if (m_pwm_light_state) {
+		client.publish(MQTT_PWM_LIGHT_STATE_TOPIC, STATE_ON, true);
+	} else {
+		client.publish(MQTT_PWM_LIGHT_STATE_TOPIC, STATE_OFF, true);
+	}
+}
+
+// function called to publish the brightness of the led
+void publishPWMLightBrightness() {
+	snprintf(m_msg_buffer, MSG_BUFFER_SIZE, "%d", m_light_brightness);
+	client.publish(MQTT_PWM_LIGHT_BRIGHTNESS_STATE_TOPIC, m_msg_buffer, true);
+}
+
+// function called to publish the state of the led (on/off)
+void publishSimpleLightState() {
+	if (m_simple_light_state) {
+		client.publish(MQTT_SIMPLE_LIGHT_STATE_TOPIC, STATE_ON, true);
+	} else {
+		client.publish(MQTT_SIMPLE_LIGHT_STATE_TOPIC, STATE_OFF, true);
+	}
+}
+
+// function called to publish the state of the PIR (on/off)
+void publishPirState() {
+	if (m_pir_state) {
+		client.publish(MQTT_MOTION_STATUS_TOPIC, STATE_ON, true);
+	} else {
+		client.publish(MQTT_MOTION_STATUS_TOPIC, STATE_OFF, true);
+	}
+	m_published_pir_state = m_pir_state;
+}
+
+// function called to publish the brightness of the led
+void publishTemperature(float temp) {
+	dtostrf(temp, 3, 2, m_msg_buffer);
+	client.publish(MQTT_TEMPARATURE_TOPIC, m_msg_buffer, true);
+}
+#if DHT_DS_MODE == DHT 
+// function called to publish the brightness of the led
+void publishHumidity(float hum) {
+	dtostrf(hum, 3, 2, m_msg_buffer);
+	client.publish(MQTT_HUMIDITY_TOPIC, m_msg_buffer, true);
+}
+#endif
+//////////////////////////////////// PUBLISHER ///////////////////////////////////////
+
+
+///////////////////// function called when a MQTT message arrived /////////////////////
+void callback(char* p_topic, byte* p_payload, unsigned int p_length) {
+	// concat the payload into a string
+	String payload;
+	for (uint8_t i = 0; i < p_length; i++) {
+		payload.concat((char)p_payload[i]);
+	}
+	// handle message topic
+	////////////////////////// SET LIGHT ON/OFF ////////////////////////
+	if (String(MQTT_PWM_LIGHT_COMMAND_TOPIC).equals(p_topic)) {
+		// test if the payload is equal to "ON" or "OFF"
+		if (payload.equals(String(STATE_ON))) {
+			if (m_pwm_light_state != true) {
+				m_pwm_light_state = true;
+				setPWMLightState();
+				publishPWMLightState();
+			}
+		} else if (payload.equals(String(STATE_OFF))) {
+			if (m_pwm_light_state != false) {
+				m_pwm_light_state = false;
+				setPWMLightState();
+				publishPWMLightState();
+			}
+		}
+	}
+	else if (String(MQTT_SIMPLE_LIGHT_COMMAND_TOPIC).equals(p_topic)) {
+		// test if the payload is equal to "ON" or "OFF"
+		if (payload.equals(String(STATE_ON))) {
+			if (m_simple_light_state != true) {
+				m_simple_light_state = true;
+				setSimpleLightState();
+				publishSimpleLightState();
+			}
+		} else if (payload.equals(String(STATE_OFF))) {
+			if (m_simple_light_state != false) {
+				m_simple_light_state = false;
+				setSimpleLightState();
+				publishSimpleLightState();
+			}
+		}
+	}
+	////////////////////////// SET LIGHT ON/OFF ////////////////////////
+	////////////////////////// SET LIGHT BRIGHTNESS ////////////////////////
+	else if (String(MQTT_PWM_LIGHT_BRIGHTNESS_COMMAND_TOPIC).equals(p_topic)) {
+		uint8_t brightness = payload.toInt();
+		if (brightness < 0 || brightness > 255) {
+			// do nothing...
+			return;
+		} else {
+			m_light_brightness = brightness;
+			setPWMLightState();
+			publishPWMLightBrightness();
+		}
+	}
+	////////////////////////// SET LIGHT BRIGHTNESS ////////////////////////
+}
+///////////////////// function called when a MQTT message arrived /////////////////////
+
+////////////////////// peripheral function ///////////////////////////////////
+#if DHT_DS_MODE == DS
+float getTemp(){ // https://blog.silvertech.at/arduino-temperatur-messen-mit-1wire-sensor-ds18s20ds18b20ds1820/
+	//returns the temperature from one DS18S20 in DEG Celsius
+
+	byte data[12];
+	byte addr[8];
+
+	if ( !ds.search(addr)) {
+		//no more sensors on chain, reset search
+		ds.reset_search();
+		return -1000;
+	}
+
+	if ( OneWire::crc8( addr, 7) != addr[7]) {
+		Serial.println("CRC is not valid!");
+		return -1000;
+	}
+
+	if ( addr[0] != 0x10 && addr[0] != 0x28) {
+		Serial.print("Device is not recognized");
+		return -1000;
+	}
+
+	ds.reset();
+	ds.select(addr);
+	ds.write(0x44,1); // start conversion, with parasite power on at the end
+
+	byte present = ds.reset();
+	ds.select(addr);    
+	ds.write(0xBE); // Read Scratchpad
+
+
+	for (int i = 0; i < 9; i++) { // we need 9 bytes
+		data[i] = ds.read();
+	}
+
+	ds.reset_search();
+
+	byte MSB = data[1];
+	byte LSB = data[0];
+
+	float tempRead = ((MSB << 8) | LSB); //using two's compliment
+	float TemperatureSum = tempRead / 16;
+
+	return TemperatureSum;  
+}
+#else 
+float getTemp(){
+	return dht.readTemperature();
+}
+
+float getHumidity(){
+	return dht.readHumidity();
+}
+#endif
+
+void updatePIRstate(){
+	m_pir_state = digitalRead(PIR_PIN);
+}
+
+// external button push
+void updateBUTTONstate(){
+	// toggle, write to pin, publish to server
+	m_simple_light_state = !m_simple_light_state;
+	setSimpleLightState();
+	publishSimpleLightState();
+}
+
+////////////////////// peripheral function ///////////////////////////////////
+
+////////////////////// network function ///////////////////////////////////
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("INFO: Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD)) {
+      Serial.println("\nINFO: connected");
+      
+      // Once connected, publish an announcement...
+      // publish the initial values
+      publishPWMLightState();
+      publishPWMLightBrightness();
+
+      // ... and resubscribe
+      client.subscribe(MQTT_PWM_LIGHT_COMMAND_TOPIC);
+      client.subscribe(MQTT_PWM_LIGHT_BRIGHTNESS_COMMAND_TOPIC);
+      client.subscribe(MQTT_SIMPLE_LIGHT_COMMAND_TOPIC);
+    } else {
+      Serial.print("ERROR: failed, rc=");
+      Serial.print(client.state());
+      Serial.println("DEBUG: try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+////////////////////// network function ///////////////////////////////////
+////////////////////// SETUP ///////////////////////////////////
+void setup() {
+	// init the serial
+	Serial.begin(115200);
+
+	// init the led
+	pinMode(PWM_LIGHT_PIN, OUTPUT);
+	analogWriteRange(255);
+	setPWMLightState();
+
+	pinMode(SIMPLE_LIGHT_PIN, OUTPUT);
+	setSimpleLightState();
+	
+
+	// init the WiFi connection
+	Serial.println();
+	Serial.println();
+	Serial.print("INFO: Connecting to ");
+	Serial.println(WIFI_SSID);
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+
+#if DHT_DS_MODE == DHT 
+	// dht
+	dht.begin();
+#endif
+
+	Serial.println("");
+	Serial.println("INFO: WiFi connected");
+	Serial.print("INFO: IP address: ");
+	Serial.println(WiFi.localIP());
+
+	// init the MQTT connection
+	client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
+	client.setCallback(callback);
+
+	// attache interrupt code for PIR
+	pinMode(PIR_PIN, OUTPUT);
+	digitalWrite(PIR_PIN,HIGH); // pull up to avoid interrupts without sensor
+	attachInterrupt(digitalPinToInterrupt(PIR_PIN), updatePIRstate, CHANGE);
+
+	// attache interrupt code for button
+	pinMode(BUTTON_INPUT_PIN, OUTPUT);
+	digitalWrite(BUTTON_INPUT_PIN,HIGH); // pull up to avoid interrupts without sensor
+	attachInterrupt(digitalPinToInterrupt(BUTTON_INPUT_PIN), updateBUTTONstate, FALLING);
+
+}
+////////////////////// SETUP ///////////////////////////////////
+////////////////////// LOOP ///////////////////////////////////
+void loop() {
+	if (!client.connected()) {
+		reconnect();
+	}
+	client.loop();
+
+	// publish pir state
+	if(m_pir_state != m_published_pir_state){
+		publishPirState();
+	}
+
+	// send periodic updates of temperature
+	if(millis()-timer>UPDATE_TEMP){
+		timer=millis();
+		// request temp here
+		publishTemperature(getTemp());
+#if DHT_DS_MODE == DHT 
+		publishHumidity(getHumidity());
+#endif
+	}
+	
+}
+////////////////////// LOOP ///////////////////////////////////
