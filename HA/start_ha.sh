c4c0d39cd711c049b8bb3474c406fffd604579d8
Kolja Windeler <Kolja.Windeler@gmail.com>
2017-01-21 15:47:54 -0600

    added helper

--- HA/start_ha.sh
+++ HA/start_ha.sh
@@ -1,3 +1,4 @@
 # run this via /etc/rc.local
+# /path/to/start_ha.sh&
 sudo screen -S "A" -d -m
 sudo screen -r "A" -X stuff "su -s /bin/bash homeassistant -c 'whoami; source /srv/homeassistant/bin/activate; hass'\n"
