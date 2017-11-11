12740c7c5696727e261c14bb832fa07c3d26ea62
KoljaWindeler <Kolja.Windeler@gmail.com>
2017-11-11 12:09:09 +0100

    added dependencies

--- JKW_MQTT_PWM_PIR_TEMP/v4_obj/src/capability.cpp
+++ JKW_MQTT_PWM_PIR_TEMP/v4_obj/src/capability.cpp
@@ -6,7 +6,7 @@
 capability::capability(){};
 
 
-bool capability::parse(unsigned char * input, uint8_t* key){
+bool capability::parse(unsigned char * input, uint8_t* key, uint8_t* dep){
 	// in: P,S,...
 	uint8_t p=0;
 	uint8_t len = 0;
@@ -29,6 +29,14 @@ bool capability::parse(unsigned char * input, uint8_t* key){
 			temp[p]=0x00;
 			//Serial.printf("parsing. input: %s vs. key %s\r\n",temp,key);
 			if(!strcmp((const char*)key,(const char*)temp)){
+				if(strcmp((const char*)dep,(const char*)"")){
+					if(ensure_dep(input,dep)){
+						logger.print(TOPIC_GENERIC_INFO, F(""),COLOR_PURPLE);
+						logger.addColor(COLOR_PURPLE);
+						Serial.printf("%s added dependency %s\r\n",key,dep);
+						logger.remColor(COLOR_PURPLE);
+					}
+				}
 				return true;
 			}
 			p=0;
@@ -36,4 +44,27 @@ bool capability::parse(unsigned char * input, uint8_t* key){
 		input++;
 	}
 	return false;
+}
+
+bool capability::parse(unsigned char * input, uint8_t* key){
+	parse(input,key,(uint8_t*)"");
+}
+
+bool capability::ensure_dep(unsigned char* input, uint8_t* dep){
+	if(parse(input,dep)){
+		return false;
+	} else {
+		// make sure there is a trailing comma
+		uint8_t i = 0;
+		while(i<100 && input[i]!=0x00){// arg hard coding
+			i++;
+		}
+		if(input[i-1]!=','){
+			input[i]=',';
+			input[i+1]=0x00;
+		}
+
+		sprintf((char*)input,"%s%s",input,dep);
+		return true;
+	}
 }
