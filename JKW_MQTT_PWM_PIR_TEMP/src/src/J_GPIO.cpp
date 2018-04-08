e394c04cb135dd3a6020b74124f3fb8566778cbb
KoljaWindeler <Kolja.Windeler@gmail.com>
2018-04-08 22:52:02 +0200

    added INPUT capability to GPIO pins

--- JKW_MQTT_PWM_PIR_TEMP/src/src/J_GPIO.cpp
+++ JKW_MQTT_PWM_PIR_TEMP/src/src/J_GPIO.cpp
@@ -1,7 +1,9 @@
 #include <J_GPIO.h>
 
+// REMEMBER: we're talking about GPIO5 and NOT Arduino D5!!
+
 // simply the constructor
-J_GPIO::J_GPIO(){};
+J_GPIO::J_GPIO(){ };
 
 // simply the destructor
 J_GPIO::~J_GPIO(){
@@ -12,31 +14,72 @@ J_GPIO::~J_GPIO(){
 // it is in charge of deciding if the object is loaded or not, but as of now it is just formwarding the
 // capability parse response. but you can override it, e.g. to return "true" everytime and your component
 // will be loaded under all circumstances
-bool J_GPIO::parse(uint8_t* config){
-	bool f=false;
-	for(uint8_t i=0; i<=16; i++){
-		sprintf(m_msg_buffer,"G%i",i);
-		if(cap.parse(config,(uint8_t*)m_msg_buffer)){
-			m_pin[i]=true; // mark pin as "in use"
-			f = true;
+bool J_GPIO::parse(uint8_t * config){
+	bool f = false;
+
+	// output
+	for (uint8_t i = 0; i <= 16; i++) {
+		// output non inverted
+		sprintf(m_msg_buffer, "GOP%i", i);
+		if (cap.parse(config, (uint8_t *) m_msg_buffer)) {
+			m_pin_out[i] = true; // mark pin as "in use"
+			m_invert[i] = false;
+			f = true;            // at least one pin  used, so keep this component alive
+			continue;
+		};
+		// output inverted
+		sprintf(m_msg_buffer, "GON%i", i);
+		if (cap.parse(config, (uint8_t *) m_msg_buffer)) {
+			m_pin_out[i] = true; // mark pin as "in use"
+			m_invert[i] = true;
+			f = true;            // at least one pin  used, so keep this component alive
+			continue;
+		};
+		// input non inverted
+		sprintf(m_msg_buffer, "GIP%i", i);
+		if (cap.parse(config, (uint8_t *) m_msg_buffer)) {
+			m_pin_in[i] = true; // mark pin as "in use"
+			m_invert[i] = false;
+			f = true;           // at least one pin  used, so keep this component alive
+			continue;
+		};
+		// input inverted
+		sprintf(m_msg_buffer, "GIN%i", i);
+		if (cap.parse(config, (uint8_t *) m_msg_buffer)) {
+			m_pin_in[i] = true; // mark pin as "in use"
+			m_invert[i] = true;
+			f = true;           // at least one pin  used, so keep this component alive
+			continue;
 		};
 	}
 	return f;
 }
 
-
-uint8_t* J_GPIO::get_key(){
+uint8_t * J_GPIO::get_key(){
 	return 0;
 }
 
-// will be callen if the m_pin is part of the config
+// will be callen if the m_pin_out is part of the config
 bool J_GPIO::init(){
-	for(uint8_t i=0; i<=16; i++){
-		if(m_pin[i]){
-			digitalWrite(i,LOW);
+	for (uint8_t i = 0; i <= 16; i++) {
+		if (m_pin_out[i]) {
 			pinMode(i, OUTPUT);
-			m_invert[i]=false;
-			sprintf(m_msg_buffer,"J_GPIO G%i init",i);
+			digitalWrite(i, m_invert[i]);
+			if(m_invert[i]){
+				sprintf(m_msg_buffer, "J_GPIO active low output on pin %i", i);
+			} else {
+				sprintf(m_msg_buffer, "J_GPIO active high output on pin %i", i);
+			}
+			logger.println(TOPIC_GENERIC_INFO, m_msg_buffer, COLOR_GREEN);
+		}
+		if (m_pin_in[i]) {
+			pinMode(i, INPUT);
+			digitalWrite(i, m_invert[i]); // inverted input gets pull-up
+			if(m_invert[i]){
+				sprintf(m_msg_buffer, "J_GPIO active low input on pin %i", i);
+			} else {
+				sprintf(m_msg_buffer, "J_GPIO active high input on pin %i", i);
+			}
 			logger.println(TOPIC_GENERIC_INFO, m_msg_buffer, COLOR_GREEN);
 		}
 	}
@@ -53,11 +96,31 @@ uint8_t J_GPIO::count_intervall_update(){
 // so you CAN run uninterrupted by returning true, but you shouldn't do that for
 // a long time, otherwise nothing else will be executed
 bool J_GPIO::loop(){
-	for(uint8_t i=0; i<=16; i++){
-		if(m_pin[i]){
-			if(millis()>=m_next_action[i] && m_next_action[i]!=0){
-				set_output(i,0);
-				m_next_action[i]=0;
+	for (uint8_t i = 0; i <= 16; i++) {
+		// output
+		if (m_pin_out[i]) {
+			if (millis() >= m_timing_parameter[i] && m_timing_parameter[i] != 0) {
+				set_output(i, 0);
+				m_timing_parameter[i] = 0;
+			}
+		}
+		// INPUT
+		else if (m_pin_in[i]) {
+			// if parameter is high: set state to 1 and increase once per second
+			// publish will send this to the server
+			// we reset this as soon as the pin goes low
+			if (digitalRead(i) != m_invert[i]) {
+				if (m_timing_parameter[i] == 0) {
+					m_state[i].set(1);
+					m_timing_parameter[i] = millis();
+				} else {
+					m_state[i].check_set(min(10, (uint8_t) ((millis() - m_timing_parameter[i]) / 1000) + 1));
+				}
+			}
+			// reset timing paramter once the pin changes again
+			else if (m_timing_parameter[i]) {
+				m_timing_parameter[i] = 0;
+				m_state[i].check_set(0);
 			}
 		}
 	}
@@ -76,27 +139,22 @@ bool J_GPIO::intervall_update(uint8_t slot){
 // this is the chance to fire some subsctions
 // return is ignored
 bool J_GPIO::subscribe(){
-	for(uint8_t i=0; i<=16; i++){
-		if(m_pin[i]){
+	for (uint8_t i = 0; i <= 16; i++) {
+		if (m_pin_out[i]) {
 			// set J_GPIO state
-			sprintf(m_msg_buffer,MQTT_J_GPIO_OUTPUT_TOPIC,i);
-			network.subscribe(build_topic(m_msg_buffer,PC_TO_UNIT)); // simple rainbow  topic
-			logger.println(TOPIC_MQTT_SUBSCIBED, build_topic(m_msg_buffer,PC_TO_UNIT), COLOR_GREEN);
+			sprintf(m_msg_buffer, MQTT_J_GPIO_OUTPUT_STATE_TOPIC, i);
+			network.subscribe(build_topic(m_msg_buffer, PC_TO_UNIT));
+			logger.println(TOPIC_MQTT_SUBSCIBED, build_topic(m_msg_buffer, PC_TO_UNIT), COLOR_GREEN);
 
 			// change J_GPIO state
-			sprintf(m_msg_buffer,MQTT_J_GPIO_TOGGLE_TOPIC,i);
-			network.subscribe(build_topic(m_msg_buffer,PC_TO_UNIT)); // simple rainbow  topic
-			logger.println(TOPIC_MQTT_SUBSCIBED, build_topic(m_msg_buffer,PC_TO_UNIT), COLOR_GREEN);
+			sprintf(m_msg_buffer, MQTT_J_GPIO_TOGGLE_TOPIC, i);
+			network.subscribe(build_topic(m_msg_buffer, PC_TO_UNIT));
+			logger.println(TOPIC_MQTT_SUBSCIBED, build_topic(m_msg_buffer, PC_TO_UNIT), COLOR_GREEN);
 
 			// pulse the output
-			sprintf(m_msg_buffer,MQTT_J_GPIO_PULSE_TOPIC,i);
-			network.subscribe(build_topic(m_msg_buffer,PC_TO_UNIT));
-			logger.println(TOPIC_MQTT_SUBSCIBED, build_topic(m_msg_buffer,PC_TO_UNIT), COLOR_GREEN);
-
-			// invert setting
-			sprintf(m_msg_buffer,MQTT_J_GPIO_INVERT_TOPIC,i);
-			network.subscribe(build_topic(m_msg_buffer,PC_TO_UNIT));
-			logger.println(TOPIC_MQTT_SUBSCIBED, build_topic(m_msg_buffer,PC_TO_UNIT), COLOR_GREEN);
+			sprintf(m_msg_buffer, MQTT_J_GPIO_PULSE_TOPIC, i);
+			network.subscribe(build_topic(m_msg_buffer, PC_TO_UNIT));
+			logger.println(TOPIC_MQTT_SUBSCIBED, build_topic(m_msg_buffer, PC_TO_UNIT), COLOR_GREEN);
 		}
 	}
 	return true;
@@ -104,69 +162,54 @@ bool J_GPIO::subscribe(){
 
 // drive pin
 void J_GPIO::set_output(uint8_t pin, uint8_t state){
-	if(pin>16){
+	if (pin > 16) {
 		return;
 	}
 	if (state) {
 		m_state[pin].set(1);
-		sprintf(m_msg_buffer,"Set J_GPIO %i: ON",pin);
+		sprintf(m_msg_buffer, "Set J_GPIO %i: ON", pin);
 	} else {
 		m_state[pin].set(0);
-		sprintf(m_msg_buffer,"Set J_GPIO %i: OFF",pin);
+		sprintf(m_msg_buffer, "Set J_GPIO %i: OFF", pin);
 	}
 	logger.println(TOPIC_MQTT, m_msg_buffer, COLOR_PURPLE);
 	bool output_state = m_state[pin].get_value();
-	if(m_invert[pin]){
+	if (m_invert[pin]) {
 		output_state = !output_state;
 	}
-	digitalWrite(pin,output_state);
+	digitalWrite(pin, output_state);
 };
 
 
 // will be called everytime a MQTT message is received, if it is for you, return true. else other will be asked.
-bool J_GPIO::receive(uint8_t* p_topic, uint8_t* p_payload){
-	for(uint8_t i=0; i<=16; i++){
-		if(m_pin[i]){
+bool J_GPIO::receive(uint8_t * p_topic, uint8_t * p_payload){
+	for (uint8_t i = 0; i <= 16; i++) {
+		if (m_pin_out[i]) {
 			// set pin once
-			sprintf(m_msg_buffer,MQTT_J_GPIO_OUTPUT_TOPIC,i);
-			if (!strcmp((const char *) p_topic, build_topic(m_msg_buffer,PC_TO_UNIT))) { // on / off with dimming
+			sprintf(m_msg_buffer, MQTT_J_GPIO_OUTPUT_STATE_TOPIC, i);
+			if (!strcmp((const char *) p_topic, build_topic(m_msg_buffer, PC_TO_UNIT))) { // on / off with dimming
 				// change OUTPUT
 				if (!strcmp_P((const char *) p_payload, STATE_ON)) {
-					set_output(i,1);
+					set_output(i, 1);
 				} else if (!strcmp_P((const char *) p_payload, STATE_OFF)) {
-					set_output(i,0);
+					set_output(i, 0);
 				}
 				return true;
 			}
 
 			// change pin
-			sprintf(m_msg_buffer,MQTT_J_GPIO_TOGGLE_TOPIC,i);
-			if (!strcmp((const char *) p_topic, build_topic(m_msg_buffer,PC_TO_UNIT))) { // on / off with dimming
-				set_output(i,!m_state[i].get_value());
+			sprintf(m_msg_buffer, MQTT_J_GPIO_TOGGLE_TOPIC, i);
+			if (!strcmp((const char *) p_topic, build_topic(m_msg_buffer, PC_TO_UNIT))) { // on / off with dimming
+				set_output(i, !m_state[i].get_value());
 				return true;
 			}
 
 			// pulse output
-			sprintf(m_msg_buffer,MQTT_J_GPIO_PULSE_TOPIC,i);
-			if (!strcmp((const char *) p_topic, build_topic(m_msg_buffer,PC_TO_UNIT))) { // on / off with dimming
-				set_output(i,1);
-				uint16_t pulse_length = atoi((char*)p_payload);
-				m_next_action[i] = millis()+pulse_length;
-				// do something
-				return true;
-			}
-
-			// invert settings
-			sprintf(m_msg_buffer,MQTT_J_GPIO_INVERT_TOPIC,i);
-			if (!strcmp((const char *) p_topic, build_topic(m_msg_buffer,PC_TO_UNIT))) { // on / off with dimming
-				if (!strcmp_P((const char *) p_payload, STATE_ON)) {
-					sprintf(m_msg_buffer,"Set J_GPIO %i: inverted",i);
-					m_invert[i] = true;
-				} else {
-					sprintf(m_msg_buffer,"Set J_GPIO %i: non-inverted",i);
-					m_invert[i] = false;
-				}
-				logger.println(TOPIC_MQTT, m_msg_buffer, COLOR_GREEN);
+			sprintf(m_msg_buffer, MQTT_J_GPIO_PULSE_TOPIC, i);
+			if (!strcmp((const char *) p_topic, build_topic(m_msg_buffer, PC_TO_UNIT))) { // on / off with dimming
+				set_output(i, 1);
+				uint16_t pulse_length = atoi((char *) p_payload);
+				m_timing_parameter[i] = millis() + pulse_length;
 				// do something
 				return true;
 			}
@@ -175,31 +218,53 @@ bool J_GPIO::receive(uint8_t* p_topic, uint8_t* p_payload){
 
 	// not for me
 	return false;
-}
+} // receive
 
 // if you have something very urgent, do this in this method and return true
 // will be checked on every main loop, so make sure you don't do this to often
 bool J_GPIO::publish(){
 	bool ret = false;
-	for(uint8_t i=0; i<=16; i++){
-		if(m_pin[i]){
-			if(m_state[i].get_outdated()){
-				sprintf(m_msg_buffer,"J_GPIO %i state ",i);
+
+	for (uint8_t i = 0; i <= 16; i++) {
+		// publish output state
+		if (m_pin_out[i] || m_pin_in[i]) {
+			if (m_state[i].get_outdated()) {
+				sprintf(m_msg_buffer, "J_GPIO %i state ", i);
 				logger.print(TOPIC_MQTT_PUBLISH, m_msg_buffer, COLOR_GREEN);
 
-				sprintf(m_msg_buffer,MQTT_J_GPIO_OUTPUT_TOPIC,i);
-				if (m_state[i].get_value()) {
-					logger.pln((char*)STATE_ON);
-					ret = network.publish(build_topic(m_msg_buffer,UNIT_TO_PC), (char*)STATE_ON);
-				} else {
-					logger.pln((char*)STATE_OFF);
-					ret = network.publish(build_topic(m_msg_buffer,UNIT_TO_PC), (char*)STATE_OFF);
+				if (m_pin_out[i]) {
+					sprintf(m_msg_buffer, MQTT_J_GPIO_OUTPUT_STATE_TOPIC, i);
+					if (m_state[i].get_value()) {
+						logger.pln((char *) STATE_ON);
+						ret = network.publish(build_topic(m_msg_buffer, UNIT_TO_PC), (char *) STATE_ON);
+					} else {
+						logger.pln((char *) STATE_OFF);
+						ret = network.publish(build_topic(m_msg_buffer, UNIT_TO_PC), (char *) STATE_OFF);
+					}
+				} else { // input pin
+					if (m_state[i].get_value() == 1 || m_state[i].get_value() == 0) { // right after push / release
+						sprintf(m_msg_buffer, MQTT_J_GPIO_OUTPUT_STATE_TOPIC, i);
+						if (m_state[i].get_value()) {
+							logger.pln((char *) STATE_ON);
+							ret = network.publish(build_topic(m_msg_buffer, UNIT_TO_PC), (char *) STATE_ON);
+						} else {
+							logger.pln((char *) STATE_OFF);
+							ret = network.publish(build_topic(m_msg_buffer, UNIT_TO_PC), (char *) STATE_OFF);
+						}
+					} else { // publishes on hold topics
+						logger.pln((char) (m_state[i].get_value()));
+						// publish
+						sprintf(m_msg_buffer, MQTT_J_GPIO_OUTPUT_HOLD_TOPIC, i);
+						sprintf(m_msg_buffer+strlen(m_msg_buffer)+1,"%i",m_state[i].get_value()-1);
+						ret = network.publish(build_topic(m_msg_buffer, UNIT_TO_PC), m_msg_buffer+strlen(m_msg_buffer)+1);
+					}
 				}
+
 				if (ret) {
 					m_state[i].outdated(false);
 				}
 				return ret; // one at the time
 			}
 		}
-	}
-}
+	} // loop
+} // publish
--- JKW_MQTT_PWM_PIR_TEMP/src/src/J_GPIO.h
+++ JKW_MQTT_PWM_PIR_TEMP/src/src/J_GPIO.h
@@ -1,33 +1,34 @@
 #ifndef J_GPIO_h
-#define J_GPIO_h
+	#define J_GPIO_h
 
 #include "main.h"
 
-static constexpr char MQTT_J_GPIO_OUTPUT_TOPIC[]   = "gpio_%i_out";
-static constexpr char MQTT_J_GPIO_TOGGLE_TOPIC[]   = "gpio_%i_toggle";
-static constexpr char MQTT_J_GPIO_PULSE_TOPIC[]    = "gpio_%i_pulse";
-static constexpr char MQTT_J_GPIO_INVERT_TOPIC[]    = "gpio_%i_invert";
+	static constexpr char MQTT_J_GPIO_OUTPUT_STATE_TOPIC[] = "gpio_%i_state";
+	static constexpr char MQTT_J_GPIO_OUTPUT_HOLD_TOPIC[]  = "gpio_%i_hold";
+	static constexpr char MQTT_J_GPIO_TOGGLE_TOPIC[]       = "gpio_%i_toggle";
+	static constexpr char MQTT_J_GPIO_PULSE_TOPIC[]        = "gpio_%i_pulse";
 
-class J_GPIO : public peripheral {
-	public:
+	class J_GPIO : public peripheral {
+public:
 		J_GPIO();
 		~J_GPIO();
 		bool init();
 		bool loop();
 		bool intervall_update(uint8_t slot);
 		bool subscribe();
-		bool parse(uint8_t* config);
+		bool parse(uint8_t * config);
 		uint8_t count_intervall_update();
-		bool receive(uint8_t* p_topic, uint8_t* p_payload);
+		bool receive(uint8_t * p_topic, uint8_t * p_payload);
 		bool publish();
-		uint8_t* get_key();
-	private:
+		uint8_t * get_key();
+private:
 		void set_output(uint8_t pin, uint8_t state);
-		bool m_pin[17]={false};
+		bool m_pin_out[17] = { false };
+		bool m_pin_in[17]  = { false };
 		mqtt_parameter_8 m_state[17];
-		uint32_t m_next_action[17]={0};
+		uint32_t m_timing_parameter[17] = { 0 };
 		bool m_invert[17];
-};
+	};
 
 
-#endif
+#endif // ifndef J_GPIO_h
