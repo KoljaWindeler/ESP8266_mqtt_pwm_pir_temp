ddcd96c06d4fbfdfcfe616d8adc60b227abe1acc
kolja windeler <kolja.windeler@gmail.com>
2018-07-23 20:44:23 +0200

    updated sprinkler

---
+++ HA/appdaemon/apps/dev29_warmwater.py
@@ -0,0 +1,22 @@
+import appdaemon.plugins.hass.hassapi as hass
+import datetime, time
+
+
+#
+# Hellow World App
+#
+# Args:
+#
+
+class warm_waterWorld(hass.Hass):
+
+    def initialize(self):
+        self.log("Starting warm_water Service")
+        self.run_daily(self.trigger, datetime.time(6, 45, 0))
+
+    def trigger(self, entity):
+        self.log("Toggeling Flow state for 10 sec")
+        self.turn_on("light.dev29")
+        time.sleep(10)
+        self.turn_off("light.dev29")
+        self.log("all done")
