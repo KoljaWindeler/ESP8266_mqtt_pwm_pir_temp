bafe5fc3b4357470c9e24bd42e6c456d282ef46e
KoljaWindeler <Kolja.Windeler@gmail.com>
2018-10-15 22:24:22 +0200

    significan click reduction

--- JKW_MQTT_PWM_PIR_TEMP/src/src/cap_play.cpp
+++ JKW_MQTT_PWM_PIR_TEMP/src/src/cap_play.cpp
@@ -2,9 +2,9 @@
 
 // simply the constructor
 play::play(){
-	buffer8b  = NULL;
-	tcp_server    = NULL;
-	connected = false;
+	buffer8b   = NULL;
+	tcp_server = NULL;
+	connected  = false;
 	sprintf((char *) key, "PLY");
 	SetGain(0.35);
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! //
@@ -33,12 +33,12 @@ play::~play(){
 // capability parse response. but you can override it, e.g. to return "true" everytime and your component
 // will be loaded under all circumstances
 bool play::parse(uint8_t * config){
-	if(cap.parse_wide(config, get_key(), &bit_mode)){
-			// should really only accept AUD8 and AUD16 but hey ..
-			if(bit_mode!=16){
-				bit_mode=8;
-			}
-			return true;
+	if (cap.parse_wide(config, get_key(), &bit_mode)) {
+		// should really only accept AUD8 and AUD16 but hey ..
+		if (bit_mode != 16) {
+			bit_mode = 8;
+		}
+		return true;
 	}
 }
 
@@ -51,7 +51,6 @@ uint8_t * play::get_key(){
 bool play::init(){
 	i2s_begin();
 	i2s_set_rate(44100);
-
 	pinMode(AMP_ENABLE_PIN, OUTPUT);
 	digitalWrite(AMP_ENABLE_PIN, LOW);
 
@@ -60,7 +59,7 @@ bool play::init(){
 
 	buffer8b = new uint8_t[BUFFER_SIZE];
 	logger.print(TOPIC_GENERIC_INFO, F("play init "), COLOR_GREEN);
-	sprintf(m_msg_buffer,"%i bit",bit_mode);
+	sprintf(m_msg_buffer, "%i bit", bit_mode);
 	logger.pln(m_msg_buffer);
 	if (buffer8b) {
 		return true;
@@ -83,7 +82,7 @@ bool play::loop(){
 	// new tcp_client?
 	if (!connected) {
 		tcp_client = tcp_server->available();
-		if(tcp_client.connected()){
+		if (tcp_client.connected()) {
 			logger.println(TOPIC_GENERIC_INFO, F("New play tcp_client"), COLOR_GREEN);
 		}
 	}
@@ -95,7 +94,7 @@ bool play::loop(){
 			// fill buffer
 			// 16384 byte buffer, 44100 hz = 371 ms playtime
 			ultimeout = millis() + 500;
-				// yield();
+			// yield();
 			do {
 				if (tcp_client.available()) {
 					buffer8b[bufferPtrIn] = tcp_client.read();
@@ -115,50 +114,50 @@ bool play::loop(){
 			digitalWrite(AMP_ENABLE_PIN, LOW); // drive low, to disable pull up
 			pinMode(AMP_ENABLE_PIN, INPUT);    // floating to reanable the sample
 			// ===================================================================================
-			ultimeout = millis() + 500;
-			connected = true;
+			ultimeout      = millis() + 500;
+			connected      = true;
 			samples_played = 0;
 			// start playback
 		}
 
 		if (connected) {
-			// playback
-			if (((bufferPtrIn - bufferPtrOut + BUFFER_SIZE) % BUFFER_SIZE) >= 2) {
-				// scale down by 4 (>>2) otherwise the output overshoots significantly
-				uint16_t t = Amplify(buffer8b[bufferPtrOut]) << 6;
-				if (bit_mode == 16) {
-					t |= Amplify(buffer8b[(bufferPtrOut + 1) % BUFFER_SIZE]) >> 2;
-				}
-				// play
-				uint32_t s32 = (t << 16) & 0xffff0000 | (t & 0xffff);
+			// playback -- keep playing the same sample on buffer underrun
+			// scale down by 4 (>>2) otherwise the output overshoots significantly
+			uint16_t t = Amplify(buffer8b[bufferPtrOut]) << 6;
+			if (bit_mode == 16) {
+				t |= Amplify(buffer8b[(bufferPtrOut + 1) % BUFFER_SIZE]) >> 2;
+			}
+			// play
+			uint32_t s32 = (t << 16) & 0xffff0000 | (t & 0xffff);
 
-				if (i2s_write_sample_nb(s32)) { // If we can't store it, return false.  OTW true
-					run_noninterrupted = false; // our chance, i2s just received new samples, see if we have to publish something
+			if (i2s_write_sample_nb(s32)) { // If we can't store it, return false.  OTW true
+				run_noninterrupted = false;    // our chance, i2s just received new samples, see if we have to publish something
+				if (((bufferPtrIn - bufferPtrOut + BUFFER_SIZE) % BUFFER_SIZE) >= 2) { // not close to buffer end, step forward
 					if (bit_mode == 16) {
 						bufferPtrOut = (bufferPtrOut + 2) % BUFFER_SIZE;
 					} else {
 						bufferPtrOut = (bufferPtrOut + 1) % BUFFER_SIZE;
 					}
+					ultimeout = millis() + 1500; // we still have data, no timeout
 				}
-				// no timeout, we still have data, playback stops once we didn't have data for 500ms
-				ultimeout = millis() + 1500;
 			}
+
 			// timeout! can stll overridden by new data
-			else if (millis() > ultimeout) {
+			if (millis() > ultimeout) {
 				connected = false;
 				logger.println(TOPIC_GENERIC_INFO, F("play timeout"), COLOR_RED);
 			}
 
 			/*
-			samples_played++;
-			if(samples_played>5000){
-				samples_played=0;
-				Serial.printf("%i\r\n",((bufferPtrIn - bufferPtrOut + BUFFER_SIZE) % BUFFER_SIZE));
-			}
-			*/
+			 * samples_played++;
+			 * if(samples_played>5000){
+			 * samples_played=0;
+			 * Serial.printf("%i\r\n",((bufferPtrIn - bufferPtrOut + BUFFER_SIZE) % BUFFER_SIZE));
+			 * }
+			 */
 
 			// read new data to buffer
-			for(uint8_t s=0; s<bit_mode; s+=8){
+			for (uint8_t s = 0; s < bit_mode; s += 8) {
 				if (tcp_client.available()) {
 					// ring-buffer free?
 					if (((bufferPtrIn + 3) % BUFFER_SIZE) != bufferPtrOut) {
@@ -170,20 +169,19 @@ bool play::loop(){
 			}
 
 			// on disconnect
-			if (!connected ) {
+			if (!connected) {
 				shutdown();
 			}
 		}
 		return run_noninterrupted; // i played music leave me running
-	} else   {
-		if(connected){
+	} else {
+		if (connected) {
 			shutdown();
 		}
 		// nnope, did nothing, go on
 		return false;
 	}
 } // loop
-
 
 // shut the AMP down via PIN, fastest and Plopp avoiding
 void play::shutdown(){
@@ -215,21 +213,31 @@ bool play::intervall_update(uint8_t slot){
 // this is the chance to fire some subsctions
 // return is ignored
 bool play::subscribe(){
-	network.subscribe(build_topic(MQTT_play_VOL_TOPIC,PC_TO_UNIT)); // simple rainbow  topic
-	logger.println(TOPIC_MQTT_SUBSCIBED, build_topic(MQTT_play_VOL_TOPIC,PC_TO_UNIT), COLOR_GREEN);
+	network.subscribe(build_topic(MQTT_play_VOL_TOPIC, PC_TO_UNIT)); // simple rainbow  topic
+	logger.println(TOPIC_MQTT_SUBSCIBED, build_topic(MQTT_play_VOL_TOPIC, PC_TO_UNIT), COLOR_GREEN);
+	network.subscribe(build_topic(MQTT_play_SR_TOPIC, PC_TO_UNIT)); // simple rainbow  topic
+	logger.println(TOPIC_MQTT_SUBSCIBED, build_topic(MQTT_play_SR_TOPIC, PC_TO_UNIT), COLOR_GREEN);
+
 	return true;
 }
 
 // will be called everytime a MQTT message is received, if it is for you, return true. else other will be asked.
 bool play::receive(uint8_t * p_topic, uint8_t * p_payload){
-	if (!strcmp((const char *) p_topic, build_topic(MQTT_play_VOL_TOPIC,PC_TO_UNIT))) { // on / off with dimming
-		int vol_precent = atoi((const char *)p_payload);
-		logger.print(TOPIC_MQTT, F("received play volume command: "),COLOR_PURPLE);
-		sprintf(m_msg_buffer,"%i%%",vol_precent);
+	if (!strcmp((const char *) p_topic, build_topic(MQTT_play_VOL_TOPIC, PC_TO_UNIT))) { // on / off with dimming
+		int vol_precent = atoi((const char *) p_payload);
+		logger.print(TOPIC_MQTT, F("received play volume command: "), COLOR_PURPLE);
+		sprintf(m_msg_buffer, "%i%%", vol_precent);
 		logger.pln(m_msg_buffer);
-		SetGain(((float)vol_precent)/100);
-	 	return true;
-	 }
+		SetGain(((float) vol_precent) / 100);
+		return true;
+	} else if (!strcmp((const char *) p_topic, build_topic(MQTT_play_SR_TOPIC, PC_TO_UNIT)))    { // on / off with dimming
+		uint16_t sample_rate = atoi((const char *) p_payload);
+		logger.print(TOPIC_MQTT, F("received sample rate command: "), COLOR_PURPLE);
+		sprintf(m_msg_buffer, "%i%%", sample_rate);
+		logger.pln(m_msg_buffer);
+		i2s_set_rate(sample_rate);
+		return true;
+	}
 	return false; // not for me
 }
 
@@ -240,14 +248,14 @@ bool play::publish(){
 }
 
 // some sort of amplification concept
-bool play::SetGain(float f) {
-	if (f>4.0){
+bool play::SetGain(float f){
+	if (f > 4.0) {
 		f = 4.0;
 	}
-	if (f<0.0){
-		f=0.0;
+	if (f < 0.0) {
+		f = 0.0;
 	}
-	gainF2P6 = (uint8_t)(f*(1<<6));
+	gainF2P6 = (uint8_t) (f * (1 << 6));
 	return true;
 }
 
