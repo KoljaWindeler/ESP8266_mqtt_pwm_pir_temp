c358bcd38ccb9a680d6f105cbe7770d51ec0b9fa
Kolja windeler <Kolja.windeler@gmail.com>
2019-05-01 21:08:16 +0200

    added connection checker

---
+++ HA/appdaemon/apps/connection.py
@@ -0,0 +1,17 @@
+import appdaemon.plugins.hass.hassapi as hass
+from datetime import datetime, time
+
+class ConnectionWorld(hass.Hass):
+
+    def initialize(self):
+        self.log("Starting Connection Service")
+        for i in range(1,60):
+            e="sensor.dev"+str(i)+"_update"
+            if(self.entity_exists(e)):
+                self.listen_state(self.t, "sensor.dev"+str(i)+"_update")
+
+    def t(self, entity, attribute, old, new,kwargs):
+        if(new==5):
+            msg="entity "+entity+" new timeout: "+str(new)
+            self.log(msg)
+            self.call_service("notify/pb", title="connection alert", message=msg)
