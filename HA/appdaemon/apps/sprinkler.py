1136164d4a400a9a871a50de76495cb4ed37a9be
Kolja windeler <Kolja.windeler@gmail.com>
2020-04-21 13:00:07 +0200

    update to statemachine to avoid sleeps

--- HA/appdaemon/apps/sprinkler.py
+++ HA/appdaemon/apps/sprinkler.py
@@ -7,175 +7,265 @@ import datetime, time
 # Args:
 #
 
+SWITCH_PUMP = "switch.dev4_gpio_12"
+SWITCH_POWER = "switch.dev30_gpio_2"
+SWITCH_VALVE1 = "switch.dev30_gpio_16"
+SWITCH_VALVE2 = "switch.dev30_gpio_4"
+SWITCH_VALVE3 = "switch.dev30_gpio_5"
+SWITCH_VALVE4 = "switch.dev30_gpio_15"
+SWITCH_VALVE5 = "switch.dev30_gpio_14"
+
 class SprinklerWorld(hass.Hass):
+	def g(self,entity,default):
+		try:
+			r = self.get_state(entity)
+			if(not(isinstance(r,str))):
+				self.call_service("notify/pb", title="Irrigation", message="Problem getting "+entity+" using default value "+default)
+				r = default
+			if(r=="unknown"):
+				self.call_service("notify/pb", title="Irrigation", message="Problem getting "+entity+" using default value "+default)
+				r = default
+		except:
+			r = default
+			self.call_service("notify/pb", title="Irrigation", message="Problem getting "+entity+" using default value "+default)
+		return r
 
-    def g(self,entity,default):
-        try:
-            r = self.get_state(entity)
-            if(not(isinstance(r,str))):
-                self.call_service("notify/pb", title="Irrigation", message="Problem getting "+entity+" using default value "+default)
-                r = default
-            if(r=="unknown"):
-                self.call_service("notify/pb", title="Irrigation", message="Problem getting "+entity+" using default value "+default)
-                r = default
-        except:
-            r = default
-            self.call_service("notify/pb", title="Irrigation", message="Problem getting "+entity+" using default value "+default)
-        return r
+	def check_stop(self):
+		if(self.g(SWITCH_PUMP,"off")=="off" or self.g("input_boolean.irrigation","off")=="off"): 
+			return True
+		else:
+			return False
 
-    def check_stop(self):
-        if(self.g("switch.dev4_gpio_12","off")=="off" or self.g("input_boolean.irrigation","off")=="off"): 
-            return True
-        else:
-            return False
-
-    def initialize(self):
-        self.log("Starting Sprinkler Service")
-        self.listen_state(self.start,"input_boolean.irrigation", new = "on")
-        self.listen_state(self.start_o,"input_boolean.irrigation_override", new = "on")
-        self.turn_off("switch.dev4_gpio_12")
-        self.run_daily(self.start_o, datetime.time(7, 0, 0))
-
-
-    def start_o(self, entity="", attribute="", old="", new="",kwargs=""):
-        # set on, if we have been started by irrigation
-        self.turn_on("input_boolean.irrigation")
-
-    def start(self, entity, attribute, old, new,kwargs):
-        #self.call_service("notify/pb", title="Irrigation", message="Starting")
-        self.log("################################################")
-        self.log("Starting Sprinker call")
-        self.set_state("sensor.dev30_state",state="Starting")
-        max_time = 15 #30
-
-        config = [
-            [['switch.dev30_gpio_15'],1], # one valve, 2xt200, regular time
-            [['switch.dev30_gpio_4'],1], # one valve, 3xt200, regular time
-            [['switch.dev30_gpio_16','switch.dev30_gpio_5'],1.5] # two valves, 150% time
-                ]
-
-        #today_max_temp = int(float(self.g("sensor.yweather_temperature_max","28")))
-        if(self.g("input_boolean.irrigation_override","off")=="off"):
-            today_max_temp = int(float(self.g("sensor.dark_sky_daytime_high_temperature_0d","28")))
-            if(today_max_temp > 28):
-                max_time = 30
-            elif(today_max_temp < 22):
-                max_time = 0
-            self.log("Today max temp is "+str(today_max_temp)+", will irrigate for "+str(max_time)+ " min")
-            rain_today = int(float(self.g("sensor.dev30_uptime","0"))/5)
-        else:
-            self.turn_off("input_boolean.irrigation_override")
-            rain_today = 0
-            self.log("irrigation override, setting time to "+str(max_time))
-
-        max_time = max(max_time - rain_today,0)
-        self.log("There was already "+str(rain_today)+" min of rain, so I'll irrigate for "+str(max_time)+" now")
-
-        if(max_time>0):
-            self.log("################################################")
-            self.log("Preparing Pump")
-            self.set_state("sensor.dev30_state",state="Preparing pump")
-            self.log("Activating valve power")
-            if(int(self.g("sensor.dev30_update","0"))>0):
-                #self.call_service("notify/pb", title="Irrigation", message="Valves offline, waiting up to 5 min")
-                self.set_state("sensor.dev30_state",state="Valve offline, wait 0/50")
-                for wait in range(0,50):
-                    time.sleep(6)
-                    self.set_state("sensor.dev30_state",state="Valve offline, wait "+str(wait)+"/50")
-                    if(int(self.g("sensor.dev30_update","0"))==0):
-                        break
-            if(int(self.g("sensor.dev30_update","0"))>0):
-                #self.call_service("notify/pb", title="Irrigation", message="Valves still offline, giving up")
-                self.set_state("sensor.dev30_state",state="Valve offline, give up")
-                exit()
-            else:
-                #self.call_service("notify/pb", title="Irrigation", message="Valves online, here we go")
-                self.set_state("sensor.dev30_state",state="Valve online")
+	def initialize(self):
+		self.log("Starting Sprinkler Service")
+		self.listen_state(self.start,"input_boolean.irrigation", new = "on")
+		self.listen_state(self.start_o,"input_boolean.irrigation_override", new = "on")
+		self.turn_off(SWITCH_PUMP)
+		self.run_daily(self.start_o, datetime.time(7, 0, 0))
 
 
-            self.turn_on("switch.dev30_gpio_2")
-            self.log("Opening all valves to reduce pump pressure")
-            self.set_state("sensor.dev30_state",state="Open all valves")
-            for ring in range(0,len(config)):
-                for sprinkler in range(0,len(config[ring][0])):
-                    self.log("Valve "+config[ring][0][sprinkler]+" open")
-                    self.turn_on(config[ring][0][sprinkler])
+	def start_o(self, entity="", attribute="", old="", new="",kwargs=""):
+		# set on, if we have been started by irrigation
+		self.turn_on("input_boolean.irrigation")
 
-            for i in range(0,3):
-                self.set_state("sensor.dev30_state",state="Ramp up "+str(i+1)+"/3, 00/40")
-                self.log("Ramping up pump "+str(i+1)+" / 3")
-                self.turn_on("switch.dev4_gpio_12")
-                time.sleep(10)
-                self.set_state("sensor.dev30_state",state="Ramp up "+str(i+1)+"/3, 10/40")
-                self.log("Wait 10/40")
-                time.sleep(10)
-                self.set_state("sensor.dev30_state",state="Ramp up "+str(i+1)+"/3, 20/40")
-                self.log("Wait 20/40")
-                time.sleep(10)
-                self.log("Wait 30/40, turn off to collect water")
-                self.set_state("sensor.dev30_state",state="Ramp up "+str(i+1)+"/3, 30/40")
-                self.turn_off("switch.dev4_gpio_12")
-                time.sleep(10)
-                self.turn_on("switch.dev4_gpio_12")
-                self.log("Wait 40/40, pump turned on again")
-                self.set_state("sensor.dev30_state",state="Ramp up "+str(i+1)+"/3, 40/40")
-                time.sleep(1)
-                if(self.check_stop()):
-                    break
+	def start(self, entity, attribute, old, new,kwargs):
+		#self.call_service("notify/pb", title="Irrigation", message="Starting")
+		self.log("################################################")
+		self.log("Starting Sprinker call")
+		self.set_state("sensor.dev30_state",state="Starting")
+		self.max_time = 15 #30 # Minutes!
 
+		self.config = [
+			[[SWITCH_VALVE4],1], # one valve, 2xt200, regular time
+			[[SWITCH_VALVE2],1], # one valve, 3xt200, regular time
+			[[SWITCH_VALVE1,SWITCH_VALVE3],1.5] # two valves, 150% time
+				]
 
+		#today_max_temp = int(float(self.g("sensor.yweather_temperature_max","28")))
+		if(self.g("input_boolean.irrigation_override","off")=="off"):
+			today_max_temp = int(float(self.g("sensor.dark_sky_daytime_high_temperature_0d","28")))
+			if(today_max_temp > 28):
+				self.max_time = 30
+			elif(today_max_temp < 22):
+				self.max_time = 0
+			self.log("Today max temp is "+str(today_max_temp)+", will irrigate for "+str(self.max_time)+ " min")
+			rain_today = int(float(self.g("sensor.dev30_uptime","0"))/5)
+		else:
+			self.turn_off("input_boolean.irrigation_override")
+			rain_today = 0
+			self.log("irrigation override, setting time to fix "+str(self.max_time)+" and ignore rain time")
 
-            self.turn_on("switch.dev4_gpio_12")
-            self.log("Pump ready")
-            time.sleep(2) # give a little time to g states
-            self.log("Closing all valves")
-            for ring in range(0,len(config)):
-                for sprinkler in range(0,len(config[ring][0])):
-                    self.turn_off(config[ring][0][sprinkler])
-                    self.log("Valve "+config[ring][0][sprinkler]+" closed")
-            self.log("Preperation completed, starting the irrigation")
-            self.log("################################################")
-            self.set_state("sensor.dev30_state",state="Preparation done")
+		self.max_time = max(self.max_time - rain_today,0)
+		self.log("There was already "+str(rain_today)+" min of rain, so I'll irrigate for "+str(self.max_time)+" now")
 
+		if(self.max_time>0):
+			self.state = 100 # Prepare pump, substep 0
+			self.sm() # start statemachine
+		else:
+			self.turn_off("input_boolean.irrigation")
 
+		
+	def sm(self, entity="", attribute="", old="", new="",kwargs=""):
+		if(int(self.state/100) == 1): # [01xx] prepare pumps
+			###### valve online?
+			if(self.state%100 == 0): # [01xx] & [xx00] = [0100]
+				self.log("################################################")
+				self.log("Preparing Pump")
+				self.set_state("sensor.dev30_state",state="Preparing pump")
+				self.log("Activating valve power")
+				# try 50x 6sec to turn of the values (very rarly they are offline right when we need them)
+				if(int(self.g("sensor.dev30_update","0"))>0):
+					#self.call_service("notify/pb", title="Irrigation", message="Valves offline, waiting up to 5 min")
+					self.set_state("sensor.dev30_state",state="Valve offline, wait 0/50")
+					self.state = 101
+					self.run_in(self.sm,6) # call in again in 6 sec
+					return
+				else:
+					self.set_state("sensor.dev30_state",state="Valve online")
+					self.state = 160
+			elif(self.state%100 <= 50): # # [01xx] & [xx<=50] = 0101 - 0150: wait cycles for valve to come online
+				if(int(self.g("sensor.dev30_update","0"))>0):
+					if(self.state%100 < 50):
+						self.set_state("sensor.dev30_state",state="Valve offline, wait "+str(self.state%100)+"/50")
+						self.state += 1
+						self.run_in(self.sm,6) # call in again in 6 sec
+						return
+					else:
+						self.set_state("sensor.dev30_state",state="Valve offline, give up")
+						self.state = 0
+				else:
+					self.set_state("sensor.dev30_state",state="Valve online")
+					self.state = 160
+			###### valve online !
+			if(self.state%100 == 60): # [01xx] & [xx60] = [0160]
+				## start of pump startup cycle
+				self.turn_on(SWITCH_POWER)
+				self.turn_on(SWITCH_PUMP)
+				time.sleep(2) # give a little time to start the pump, else check_stop will stop us imidiatly
+				self.log("Opening all valves to reduce pump pressure")
+				self.set_state("sensor.dev30_state",state="Open all valves")
+				for ring in range(0,len(self.config)):
+					for sprinkler in range(0,len(self.config[ring][0])):
+						self.log("Valve "+self.config[ring][0][sprinkler]+" open")
+						self.turn_on(self.config[ring][0][sprinkler])
+				
+				self.state = 161
+			
+			###### pump starting
+			if(int(self.state/10) >= 16): # # [01xx] & [x>=160] = 160-199
+				round = int(self.state/10)-16 # 160->0, 170->1, 180->2
+				on_off = self.state%2 # 161->1->on, 162->0->off
+				sec = ((self.state%10)-1)*10 # 161 = (1-1)*10 = 0 // 162 = (2-1)*10 = 10
+				
+				self.set_state("sensor.dev30_state",state="Ramp up "+str(round+1)+"/3, "+str(sec)+"/40")
+				self.log("Ramp up "+str(round+1)+"/3, "+str(sec)+"/40")
+				if(on_off == 1):
+					self.turn_on(SWITCH_PUMP)
+				else:
+					# it should be on, but it is off .. hmm 
+					if(self.check_stop()):
+						print("ohohO 222")
+						print(self.g(SWITCH_PUMP,"off"))
 
-            for ring in range(0,len(config)):
-                if(not(self.check_stop())):
-                    self.log("Turn ring "+str(ring+1)+" on.")
-                    D = max_time * config[ring][1]
-                    for sprinkler in range(0,len(config[ring][0])):
-                        self.turn_on(config[ring][0][sprinkler])
-                    while D > 0:
-                        self.log("Currently running ring "+str(ring+1)+" for further "+str(D)+" min")
-                        self.set_state("sensor.dev30_state",state="Ring "+str(ring+1)+" -> "+str(D)+" min")
-                        D = D-1
-                        if(self.check_stop()):
-                            self.call_service("notify/pb", title="Irrigation", message="Pump off, stopping")
-                            self.log("Pump off, stopping irrigation")
-                            self.set_state("sensor.dev30_state",state="Stopping")
-                            break
-                        time.sleep(60)
-                    self.log("Turn ring "+str(ring+1)+" off.")
-                    for sprinkler in range(0,len(config[ring][0])):
-                        self.turn_off(config[ring][0][sprinkler])
-                else:
-                    self.log("Skipping ring "+str(ring+1)+" pump is off")
+						self.state = 4000 # finish up
+						self.run_in(self.sm,1) # call in again in 1 sec to finish up
+						return
 
-            self.set_state("sensor.dev30_state",state="Shutting down")
-            self.log("Shutting down valve power")
-            self.turn_off("switch.dev30_gpio_2")
-            self.log("Shutting down pump")
-            self.turn_off("switch.dev4_gpio_12")
-            #self.call_service("notify/pb", title="Irrigation", message="All done")
+					self.turn_off(SWITCH_PUMP)
+				
+				if(self.state%10==4): # 164 => round 0, on_off 0(off), sec 30
+					if(int(self.state/10)==18): # 160,170, 180
+						self.state = 200 # run irigation
+					else: # continues with ramp up
+						self.state = int(self.state/10)*10+11 # 171 => round 1, on_off 1 on, sec 0
+				else:
+					self.state += 1 # same round, toggle on_off, inc sec by 10
+				
+				self.run_in(self.sm,10) # call in again in 10 sec
+				return
+			
+		########## finish preparation
+		if(self.state == 200): # [0200]
+			self.turn_on(SWITCH_PUMP)
+			self.log("Pump ready")
+			time.sleep(2) # give a little time to g states
+			self.log("Closing all valves")
+			for ring in range(0,len(self.config)):
+				for sprinkler in range(0,len(self.config[ring][0])):
+					self.turn_off(self.config[ring][0][sprinkler])
+					self.log("Valve "+self.config[ring][0][sprinkler]+" closed")
+			self.log("Preperation completed, starting the irrigation")
+			self.log("################################################")
+			self.set_state("sensor.dev30_state",state="Preparation done")
+			self.state = 3000
+		
+		########## start of irigation
+		if(int(self.state/1000) == 3): # [3xxx]
+			ring = int(self.state/100)%10 # 3[ring][minute (2 digits)] = 3000 -> ring 0, 0 min on // 3010 -> ring 0, 10 min on
+			min = self.state%100
+			if(not(self.check_stop())):
+				# calc time D = time in minues that this ring should run
+				D = self.max_time * self.config[ring][1]
+				if(min==0):
+					# turn values on
+					self.log("Turn ring "+str(ring+1)+" on.")
+					for sprinkler in range(0,len(self.config[ring][0])):
+						self.turn_on(self.config[ring][0][sprinkler])
+					self.set_state("sensor.dev30_state",state="Ring "+str(ring+1)+" -> "+str(D-min)+" min")
+					self.state += 1
+					self.run_in(self.sm,60) # call in again in 60 sec
+					return
+				else:
+					# we're already sprinkling
+					self.log("Currently running ring "+str(ring+1)+" for further "+str(D-min)+" min")
+					self.set_state("sensor.dev30_state",state="Ring "+str(ring+1)+" -> "+str(D-min)+" min")
+					if(self.check_stop()):
+						self.state = 0
+						self.call_service("notify/pb", title="Irrigation", message="Pump off, stopping")
+						self.log("Pump off, stopping irrigation")
+						self.set_state("sensor.dev30_state",state="Stopping")
+						self.state = 4000 # finish up
+						self.run_in(self.sm,1) # call in again in 1 sec to finish up
+						return
+					else:
+						# advance state
+						if(D-min<=0):
+							# this was the last minute, close ring
+							self.log("Turn ring "+str(ring+1)+" off.")
+							for sprinkler in range(0,len(self.config[ring][0])):
+								self.turn_off(self.config[ring][0][sprinkler])
 
-        #else:
-            #self.call_service("notify/pb", title="Irrigation", message="Skipping, no rain needed")
+							# was this the last ring?
+							if(ring == len(self.config)):
+								# yes, all done, finish up
+								self.state = 4000
+							else:
+								#no, next ring
+								self.state = 3000+(ring+1)*100
+								self.run_in(self.sm,1) # call in again in 1 sec
+								return
+						else:
+							# keep running
+							self.state += 1
+							self.run_in(self.sm,60) # call in again in 60 sec
+							return
+			else:
+				# check stop hit true: shut down
+				self.log("Skipping ring "+str(ring+1)+" pump is off")
+				self.state = 4000 # finish up
+		
+		########## finish up
+		if(int(self.state/1000) == 4):
+		
+			# shut power down
+			self.set_state("sensor.dev30_state",state="Shutting down")
+			self.log("Shutting down valve power")
+			self.turn_off(SWITCH_POWER)
+			self.log("Shutting down pump")
+			self.turn_off(SWITCH_PUMP)
 
-        self.turn_on("switch.dev30_reset_rain_time")
-        time.sleep(1)
-        self.turn_off("switch.dev30_reset_rain_time")
-        self.log("All done, enjoy your evening")
-        self.turn_off("input_boolean.irrigation")
-        self.log("All done, enjoy your evening")
-        self.set_state("sensor.dev30_state",state="Done")
-        self.log("################################################")
+			# shut down all values
+			for ring in range(0,len(self.config)):
+				for sprinkler in range(0,len(self.config[ring][0])):
+					self.turn_off(self.config[ring][0][sprinkler])
+					self.log("Valve "+self.config[ring][0][sprinkler]+" closed")
+
+			# toggle rain time reset
+			self.turn_on("switch.dev30_reset_rain_time")
+			time.sleep(1)
+			self.turn_off("switch.dev30_reset_rain_time")
+
+			# finish
+			self.log("All done")
+			self.turn_off("input_boolean.irrigation")
+			self.log("All done, enjoy your evening")
+			self.set_state("sensor.dev30_state",state="Done")
+			self.log("################################################")
+			return
+						
+						
+						
+						
+						
+						
+						
